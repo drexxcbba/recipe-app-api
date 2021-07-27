e096f4937d1837ca6414702074431d118c970303
unknown <villarroel24kyle@gmail.com>
2021-07-27 13:57:43 -0400

    added token api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
@@ -13,3 +14,28 @@ class UserSerializer(serializers.ModelSerializer):
         def create(self, validated_data):
             """Create a new user with password and return it"""
             return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and auth the user"""
+        email = attrs.get('email')
+        pasword = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=pasword
+        )
+        if not user:
+            msg = ('Unable to auth with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs('user') = user
+        return attrs
