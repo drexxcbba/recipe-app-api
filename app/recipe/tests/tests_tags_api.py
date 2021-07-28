2189d7273e3d30769e80568ea6f86c24479e68da
unknown <villarroel24kyle@gmail.com>
2021-07-28 14:36:06 -0400

    fixed test

--- app/recipe/tests/tests_tags_api.py
+++ app/recipe/tests/tests_tags_api.py
@@ -58,7 +58,7 @@ class PrivateTagApiTests(TestCase):
         res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, 1)
+        self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
 
     def test_create_tag_successful(self):
